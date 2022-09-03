16171d0ed1da6d5f55645b7c497a6d479577923b
Skonteye <98233015+SKonteye@users.noreply.github.com>
2022-09-03 13:09:56 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const hourEl = document.getElementById("hour");
 const minuteEl = document.getElementById("minutes");
 const secondEl = document.getElementById("seconds");
 const ampmEl = document.getElementById("ampm");
-const bgrd = document.getElementById("body");
 
 
 function updateClock() {
@@ -13,7 +12,7 @@ function updateClock() {
 
     if (h>12) {
         h = h - 12; 
-        ampm = "PM" ;     
+        ampm = "PM";     
     }
 
     h = h < 10 ? "0" + h : h;
@@ -28,9 +27,5 @@ function updateClock() {
     setTimeout(()=>{
         updateClock()
     }, 1000 )
-    if (h==12) {
-       bgrd = document.getElementById("body").style.background =url("https://images.unsplash.com/photo-1516569333566-9d463ccffd20?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlJTIwbmlnaHR8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60");
-        
-    }
 }
 updateClock();
